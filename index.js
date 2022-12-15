cf4f0c011a7deffcd53376b87cbe2428df5276aa
Terdanny1 <terdanconcept68@gmail.com>
2022-12-15 19:16:18 +0100

    added background images to each sound

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ var numberofDrumButtons = document.querySelectorAll(".drum").length
 for (let i = 0; i < numberofDrumButtons; i++) {
     
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        alert("i just got clicked")
+       
+        var audio = new Audio("sounds/tom-1.mp3")
+        audio.play()
     })
     
 }
